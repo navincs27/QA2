124a3d4bcf8584ee2fc809669e7b43dfd8989b5a
gvshalini92@gmail.com <gvshalini92@gmail.com>
2023-12-28 12:26:13 +0530

    Element added in flight page

--- src/main/java/com/IPT_QA2_Goibibo/pom/FlightPage.java
+++ src/main/java/com/IPT_QA2_Goibibo/pom/FlightPage.java
@@ -1,5 +1,19 @@
 package com.IPT_QA2_Goibibo.pom;
 
-public class FlightPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.IPT_QA2_Goibibo.base.BaseClass;
+
+
+
+public class FlightPage extends BaseClass {
+	public FlightPage (){
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath="//input[@placeholder='Enter Last Name']")
+	private WebElement last_Name;
 
 }
