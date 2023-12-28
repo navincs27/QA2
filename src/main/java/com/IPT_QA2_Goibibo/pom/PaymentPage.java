465a714882fa1b4064089bc1da4d16c42d12027a
username <useremail>
2023-12-28 17:10:08 +0530

    done

--- src/main/java/com/IPT_QA2_Goibibo/pom/PaymentPage.java
+++ src/main/java/com/IPT_QA2_Goibibo/pom/PaymentPage.java
@@ -2,19 +2,20 @@ package com.IPT_QA2_Goibibo.pom;
 
 public class PaymentPage {
 	
-<<<<<<< HEAD
+
  public void name() {
 	 
 	 System.out.println("hi world");
 }
  
-=======
+
 	private void Payment12() {
 		System.out.println("bhartthi belongs to QA");
+		System.out.println("changes by kavi");
 		
 
 		
 	}
 
->>>>>>> bharathi
+
 }
