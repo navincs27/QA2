c34c21d808c6ee57392d04d7cb8e45922ea417b7
nilaphoenix <elanila105@gmail.com>
2023-12-28 17:14:17 +0530

    added elements for flight

--- src/main/java/com/IPT_QA2_Goibibo/pom/FlightPage.java
+++ src/main/java/com/IPT_QA2_Goibibo/pom/FlightPage.java
@@ -1,14 +1,26 @@
 package com.IPT_QA2_Goibibo.pom;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class FlightPage {
-	
+import com.IPT_QA2_Goibibo.base.BaseClass;
 
+public class FlightPage extends BaseClass {
+	public  FlightPage() {
+	PageFactory.initElements(driver, this);
 
-PageFactory.initElements(driver,this);
+	}
 
 @FindBy(xpath ="(//p[text()='Enter city or airport'])[1]")
+public WebElement flight;
+public WebElement getFlight() {
+	return flight;
+}
+@FindBy(xpath = "(//p[text()='Enter city or airport'])[2]")
+public WebElement flightto;
+public WebElement getFlightto() {
+	return flightto;
+}
 
 }
