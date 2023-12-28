d1f61b24184b96d002b464abe13be1381ab61f24
gvshalini92@gmail.com <gvshalini92@gmail.com>
2023-12-28 15:25:54 +0530

    Home and browser

--- src/main/java/com/IPT_QA2_Goibibo/pom/HomePage.java
+++ src/main/java/com/IPT_QA2_Goibibo/pom/HomePage.java
@@ -1,5 +1,26 @@
 package com.IPT_QA2_Goibibo.pom;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.IPT_QA2_Goibibo.base.BaseClass;
+
+public class HomePage extends BaseClass {
+	
+		public HomePage (){
+			PageFactory.initElements(driver, this);
+		}
+
+		@FindBy(xpath="//a[@href='/hotels/']")
+		private WebElement Hotels;
+		
+		public WebElement getHotels() {
+			return Hotels;
+		}
+
+		public void setHotels(WebElement hotels) {
+			Hotels = hotels;
+		}
 
 }
